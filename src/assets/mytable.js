e1a558738b579356297605a9f1bd76f6939c45f3
jackshawn <jackshawn@live.com>
2017-06-09 16:40:51 +0800

    modify mytable.js

--- src/assets/mytable.js
+++ src/assets/mytable.js
@@ -20,20 +20,8 @@ import 'style-loader!css-loader!./mytable.css'
         var thead = table.find('thead>tr');
         var tbody = table.find('tbody');
         var trModel = $('<tr></tr>');
-        var page = $('<div style="text-align: center"></div>');
-        var $loading = $('<tr><td colspan="' + option.thead.length + '" style="text-align: center;margin: 30px 0;font-size: 18px"></td></tr>')
-        var timr = (function () {
-          var t = 0,
-            text = '数据加载中...';
-          return setInterval(function () {
-            $loading.find('td').text(text.substr(0, t + 5));
-            $loading.appendTo(tbody)
-            if (t === 3) {
-              t = 0;
-            }
-            t++
-          }, 1000)
-        })()
+        var page = $('<div class="my-page"></div>');
+        var $loading = $('<tr><td colspan="' + option.thead.length + '" class="my-loading">loading...</td></tr>')
         
         /*填充表格*/
         var setTable = function (data) {
@@ -52,7 +40,6 @@ import 'style-loader!css-loader!./mytable.css'
           })
         }
         
-        
         /*设置th*/
         $.each(option.thead, function (i, item) {
           var th = $('<th class="' + item.key + '">' + item.name + '</th>');
@@ -67,16 +54,16 @@ import 'style-loader!css-loader!./mytable.css'
         /*排序*/
         thead.on('click', 'th', function () {
           var $span = $(this).find('.th-sort')
-          if($span.length){
+          if ($span.length) {
             var order = $(this).attr('class');
             var state = $span.find('span').eq(0).css('visibility')
-            thead.find('span').css('visibility','visible')
-            if( state !== 'visible'){
-              $span.find('span').eq(0).css('visibility','visible')
-              $span.find('span').eq(1).css('visibility','hidden')
-            }else{
-              $span.find('span').eq(0).css('visibility','hidden')
-              $span.find('span').eq(1).css('visibility','visible')
+            thead.find('span').css('visibility', 'visible')
+            if (state !== 'visible') {
+              $span.find('span').eq(0).css('visibility', 'visible')
+              $span.find('span').eq(1).css('visibility', 'hidden')
+            } else {
+              $span.find('span').eq(0).css('visibility', 'hidden')
+              $span.find('span').eq(1).css('visibility', 'visible')
             }
             option.oriData.order = order;
             init();
@@ -85,6 +72,7 @@ import 'style-loader!css-loader!./mytable.css'
         
         /*初始化方法*/
         var init = function () {
+          tbody.empty().append($loading)
           $.ajax({
             type: option.type,
             url: option.url,
@@ -92,7 +80,6 @@ import 'style-loader!css-loader!./mytable.css'
             dataType: "json",
             contentType: "application/json;charset=utf-8",
             success: function (d) {
-              clearInterval(timr)
               setTable(d.data);
               page.mypage(option.oriData.page, d.totalPage, d.count, function (index) {
                 option.oriData.page = index;
@@ -100,7 +87,6 @@ import 'style-loader!css-loader!./mytable.css'
               });
             },
             error: function (jqXHR) {
-              clearInterval(timr)
               $loading.find('td').text('加载失败，发生错误：' + jqXHR.status);
               $loading.appendTo(tbody)
               console.log("发生错误：" + jqXHR.status);
@@ -117,74 +103,62 @@ import 'style-loader!css-loader!./mytable.css'
     },
     mypage: function (curr, total, count, fn) {
       return $(this).each(function () {
-        var $this = $(this).css({color: '#666', font: '12px/26px ""', textAlign: 'center'}).empty();
-        var btnType = {
-          active: {background: '#337ab7', color: '#fff'},
-          normal: {background: '#f1eff0', color: '#666'}
+        var $this = $(this).empty();
+        var creatBtn = function (text, type) {
+          var $btn = $('<span></span>');
+          $btn.text(text).data('curr', curr);
+          if (type) {
+            $btn.addClass('active')
+          }
+          return $btn;
         };
-        var btn = function (option) {
-          var style = {
-            display: 'inline-block',
-            padding: '0 12px',
-            margin: '6px 3px',
-            borderRadius: '2px',
-            cursor: 'pointer'
-          };
-          return $('<span></span>').text(option.num)
-            .addClass(option.class || 'page-btn')
-            .data('curr', option.curr || '')
-            .css($.extend(true, style, btnType[option.type || 'normal']))
-        };
-        $this.off().on('click', 'span.page-btn', function (e) {
+        $this.off().on('click', 'span', function (e) {
           e.stopPropagation()
           var text = $(this).text();
-          var index = ($(this).data('curr') || text) * 1;
+          var index;
           switch (text) {
             case '<':
-              index--;
+              index = $(this).data('curr') - 1;
               break;
             case '>':
-              index++;
+              index = $(this).data('curr') + 1;
               break;
             default:
+              index = text * 1;
               break;
           }
-          if (fn) fn(index);
+          if (index && fn) fn(index);
         })
         /*存在分页*/
         if (total && total > 1) {
           /*显示上一页*/
           if (curr > 1) {
-            $this.append(btn({num: '<', curr: curr}));
+            $this.append(creatBtn('<'));
           }
           /*首页 1 */
           if (curr - 2 > 1) {
-            $this.append(btn({num: 1}));
-            if (curr != 4) {
-              $this.append(btn({num: '...', 'class': '...'}));
+            $this.append(creatBtn(1));
+            if (curr !== 4) {
+              $this.append(creatBtn('...'));
             }
           }
           for (var i = (curr - 2); i <= (curr + 2); i++) {
             if (i > 0 && i <= total) {
-              $this.append(btn({
-                num: i,
-                type: i == curr ? 'active' : 'normal',
-                'class': i == curr ? 'curr-btn' : ''
-              }));
+              $this.append(i === curr ? creatBtn(i, true) : creatBtn(i));
             }
           }
           /*尾页 total*/
           if (curr + 2 < total) {
-            if (total - curr != 3) {
-              $this.append(btn({num: '...', 'class': '...'}));
+            if (total - curr !== 3) {
+              $this.append(creatBtn('...'));
             }
-            $this.append(btn({num: total}));
+            $this.append(creatBtn(total));
           }
           /*显示统计*/
-          $this.append(btn({num: curr + '/' + total + '页, ' + count + '条 ', 'class': 'count'}));
+          $this.append(creatBtn(curr + '/' + total + '页, ' + count + '条 '));
           /*显示下一页*/
-          if (curr != total) {
-            $this.append(btn({num: '>', curr: curr}));
+          if (curr !== total) {
+            $this.append(creatBtn('>'));
           }
         }
       });
